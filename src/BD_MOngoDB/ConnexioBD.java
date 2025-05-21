f5728803469c0261a47f2b164a2079fe8638d16d
gerard-inspedralbes <122524717+gerard-inspedralbes@users.noreply.github.com>
2025-05-21 10:02:59 +0200

    Afegit esquelet per connexió a MongoDB

--- src/BD_MOngoDB/ConnexioBD.java
+++ src/BD_MOngoDB/ConnexioBD.java
@@ -2,8 +2,6 @@ package src.BD_MOngoDB;
 
 
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class ConnexioBD {
